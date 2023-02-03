c452fc0f748def0022a2e47f64d5348196f91b7f
Shpm21 <galleguilloscristian2@gmail.com>
2023-02-02 21:37:11 -0300

    Create aux component PopupsPolygonInformation.vue

---
+++ src/components/PopupsPolygonInformation.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-overlay
+    :absolute="absolute"
+    :value="viewPolygonsInformation"
+  >
+    <PolygonInformation :establishment='establishment' :polygons='polygons' />
+    <v-btn
+      color="success"
+      @click="viewPolygonsInformation = false"
+    >
+      Cerrar
+    </v-btn>
+  </v-overlay>
+</template>
+
+<script>
+import PolygonInformation from '@/components/PolygonInformation.vue';
+
+export default {
+  name: 'PopupsPolygonInformation',
+  components: { PolygonInformation },
+  props: {
+    polygons: {
+      type: Array,
+      required: true,
+    },
+    establishment: {
+      type: Object,
+      required: true,
+    },
+    viewPolygonsInformation: {
+      type: Boolean,
+      required: true,
+    },
+    absolute: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
