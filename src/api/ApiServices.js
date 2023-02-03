a47dc38d3909d7fe2651ca1baada17f481dc9864
Shpm21 <galleguilloscristian2@gmail.com>
2023-02-03 15:21:53 -0300

    add baseUrl

--- src/api/ApiServices.js
+++ src/api/ApiServices.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'https://mma-api.azurewebsites.net/api/';
+// const baseUrl = 'https://mma-api.azurewebsites.net/api/';
+const baseUrl = 'http://localhost:9000/';
 export const getEstablishmentsByVuCode = async (codigo_vu) => {
   return await axios.get(`${baseUrl}establecimientos/${codigo_vu}`);
 }
