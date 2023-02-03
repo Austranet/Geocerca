b3fd8147cbf4187b3e2ea96511a1a32454a88d7e
Shpm21 <galleguilloscristian2@gmail.com>
2023-02-03 20:33:25 -0300

    edit view from polygon information

---
+++ src/components/PanelDefault.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class='panel-title'>
+        {{title}}
+      </h3>
+    </div>
+    <div class="panel-body">{{content}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PanelDefault',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  }
+};
+</script>
+
+<style scoped>
+.panel {
+  margin-bottom: 20px;
+  background-color: #fff;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  -webkit-box-shadow: 0 1px 1px rgba(0,0,0,.05);
+  box-shadow: 0 1px 1px rgba(0,0,0,.05);
+}
+
+.panel panel-default {
+  border-color: #ddd;
+}
+
+.panel .panel-heading {
+  color: #333;
+  background-color: #f5f5f5;
+  border-color: #ddd;
+  text-align: center;
+}
+
+.panel .panel-heading .panel-title {
+  font-size: 14px;
+}
+
+.panel .panel-body {
+  padding: 15px;
+}
+
+</style>
---
+++ src/components/TableOfPolygons.vue
@@ -0,0 +1,64 @@
+<template>
+  <table class='table table-striped table-bordered table-hover'>
+    <thead>
+    <tr>
+      <th>CODIGO VU</th>
+      <th>ID POLIGONO</th>
+      <th>ID COORDENADA</th>
+      <th>LATITUD</th>
+      <th>LONGITUD</th>
+      <th>ESTE</th>
+      <th>NORTE</th>
+    </tr>
+    </thead>
+    <tbody v-for='(polygon, index) in polygons' :key='index'>
+    <tr v-for='(coordinate, index) in polygon.coordinates' :key='index'>
+      <td>{{codigo_vu}}</td>
+      <td>{{polygon.id}}</td>
+      <td>{{coordinate.id}}</td>
+      <td>{{coordinate.latitude}}</td>
+      <td>{{coordinate.longitude}}</td>
+      <td>{{coordinate.easting}}</td>
+      <td>{{coordinate.northing}}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'TableOfPolygons',
+  props: {
+    polygons: {
+      type: Array,
+      required: true,
+    },
+    codigo_vu: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*//haz la tabla parecida a excel*/
+.table {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 1rem;
+  background-color: transparent;
+}
+
+.table th,
+.table td {
+  padding: 0.75rem;
+  vertical-align: top;
+  border-top: 1px solid #dee2e6;
+}
+
+.table thead th {
+  vertical-align: bottom;
+  border-bottom: 2px solid #dee2e6;
+}
+</style>
